35f6d7528e6cd9e62605f45ba32135e98e6de426
Willlasbury <asburyw229@gmail.com>
2023-04-28 09:50:22 -0700

    added not null to columns in schema:

--- db/schema.sql
+++ db/schema.sql
@@ -5,14 +5,14 @@ USE company;
 
 CREATE TABLE department(
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(30)
+    name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    title VARCHAR(30),
-    salary DECIMAL,
-    department_id INT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
     FOREIGN key (department_id) 
     REFERENCES department(id)
     ON DELETE SET NULL
@@ -20,9 +20,9 @@ CREATE TABLE role (
 
 CREATE TABLE employee (
         id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-        first_name VARCHAR(30),
-        last_name VARCHAR(30),
-        role_id INT,
+        first_name VARCHAR(30) NOT NULL,
+        last_name VARCHAR(30) NOT NULL,
+        role_id INT NOT NULL,
         manager_id INT,
         Foreign Key (role_id) REFERENCES role(id),
         Foreign Key (manager_id) REFERENCES employee(id)
