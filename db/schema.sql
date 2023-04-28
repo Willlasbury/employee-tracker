4c5b1ae731a64d45c29cf06a2f8a1b1c9c06f1e9
Willlasbury <asburyw229@gmail.com>
2023-04-28 09:50:59 -0700

    formatting schema"

--- db/schema.sql
+++ db/schema.sql
@@ -1,24 +1,27 @@
 DROP DATABASE IF EXISTS company;
+
 CREATE DATABASE company;
 
 USE company;
 
-CREATE TABLE department(
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(30) NOT NULL
-);
+CREATE TABLE
+    department(
+        id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        name VARCHAR(30) NOT NULL
+    );
 
-CREATE TABLE role (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL NOT NULL,
-    department_id INT NOT NULL,
-    FOREIGN key (department_id) 
-    REFERENCES department(id)
-    ON DELETE SET NULL
-);
+CREATE TABLE
+    role (
+        id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        title VARCHAR(30) NOT NULL,
+        salary DECIMAL NOT NULL,
+        department_id INT NOT NULL,
+        FOREIGN key (department_id) REFERENCES department(id) ON DELETE
+        SET NULL
+    );
 
-CREATE TABLE employee (
+CREATE TABLE
+    employee (
         id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
         first_name VARCHAR(30) NOT NULL,
         last_name VARCHAR(30) NOT NULL,
@@ -26,4 +29,4 @@ CREATE TABLE employee (
         manager_id INT,
         Foreign Key (role_id) REFERENCES role(id),
         Foreign Key (manager_id) REFERENCES employee(id)
-)
+    )
