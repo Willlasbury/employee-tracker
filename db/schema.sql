08c296abd80b5be7b9fc28ea7a0f7f12e880850f
Willlasbury <asburyw229@gmail.com>
2023-04-28 09:28:29 -0700

    added schema.sql and table design

---
+++ db/schema.sql
@@ -0,0 +1,30 @@
+DROP DATABASE IF EXISTS database;
+CREATE DATABASE database;
+
+USE database;
+
+CREATE TABLE department(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(30)
+);
+
+CREATE TABLE role (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT
+    FOREIGN key (department_id) 
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+        id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        first_name VARCHAR(30),
+        last_name VARCHAR(30),
+        role_id INT,
+        manager_id INT
+        Foreign Key (role_id) REFERENCES role(id)
+        Foreign Key (manager_id) REFERENCES employee(id)
+
+)
